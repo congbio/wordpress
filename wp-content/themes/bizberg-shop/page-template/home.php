c59e7beb14f008462ffc3ac51f9da52a8828a83f
A Công <acongkplpe@gmail.com>
2022-11-14 15:36:02 +0700

    upload

--- wp-content/themes/bizberg-shop/page-template/home.php
+++ wp-content/themes/bizberg-shop/page-template/home.php
@@ -12,22 +12,26 @@ bizberg_shop_get_slider();
 do_action( 'bizberg_shop_before_top_categories_section' );
 bizberg_shop_get_top_categories();
 
+ 
+renderProduct();
+
+
 do_action( 'bizberg_shop_before_services_section' );
 bizberg_shop_get_services();
 
 do_action( 'bizberg_shop_before_sales_banner_section' );
-bizberg_shop_get_sales_banner(); 
+bizberg_shop_get_sales_banner();
 
 do_action( 'bizberg_shop_before_woocommerce_tab_products' );
-bizberg_shop_get_homepage_products(); 
+bizberg_shop_get_homepage_products();
 
 do_action( 'bizberg_shop_before_repeater_products' );
 bizberg_shop_get_repeater_products();
 
 do_action( 'bizberg_shop_before_clients_logo' );
-bizberg_shop_get_clients_logo(); 
+bizberg_shop_get_clients_logo();
 
-do_action( 'bizberg_shop_before_footer' ); 
+do_action( 'bizberg_shop_before_footer' );
 ?>
 <!-- <script src="../showheh.js"></script> -->
 <?php get_footer();
